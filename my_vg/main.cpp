15c618962fb607629e05668e499d2193b4add9e2
Heather Savoy <frystacka@berkeley.edu>
2015-04-17 15:54:16 -0700

    changed to vectors and uploaded my output

--- my_vg/main.cpp
+++ my_vg/main.cpp
@@ -19,54 +19,51 @@ struct ptPair {
   double dist;
 } ;
 
-int get_num_pairs(char* file);
-void read_data(char*, point* data);
+int get_num_pairs(string file);
+void read_data(string file, vector<point>* data);
 double calc_dist(point* pt1, point* pt2);
-void register_pairs(int numPoints, point* points, vector<ptPair>* pairs);
+void register_pairs(int numPoints, vector<point>* points, vector<ptPair>* pairs);
 double* find_dist_range(vector<ptPair>* pairs);
 int get_bin(double dist, double* range, int numBins);
 //void fill_variogram(vector<ptPair> pairs, double* gamma);
-void print_gamma(double* values, int* counts, double* dists, int numBins, char* filename);
+void print_gamma(vector<double>* values, vector<int>* counts, vector<double>* dists, int numBins, string filename);
 
 
 int main(int argc, char* argv[])
 {
-    //cout << "Hello world!" << endl;
-    char *inFile = argv[1];
+    string inFile;
+    inFile = argv[1];
     //1. Read in measurements (value and coordinates)
     int numPoints = get_num_pairs(inFile);
-    //cout << "Number of points: " << numPoints << endl;
-    point *data = new point[numPoints];  //use vector?
-    read_data(inFile, data);
-    //for(int i = 0; i < numPoints; i++)
-    //    cout << data[i].value << " is at " << data[i].x << "," << data[i].y << endl;
+    //point *data = new point[numPoints];  //use vector?
+    vector<point> data (numPoints);
+    read_data(inFile, &data);
     //2. Determine all the pairs of points
     vector<ptPair> allPairs;
-    register_pairs(numPoints, data, &allPairs);
-    ////cout << "First pair is this far apart: " << allPairs[0].dist << "\n";
+    register_pairs(numPoints, &data, &allPairs);
     //3. For each pair of points
     double *range = find_dist_range(&allPairs);
-    cout << "Range: " << range[0] << "," << range[1] << "\n";
     int numBins = 15;
-    double *gamma = new double[numBins];
-    int *numPairs = new int[numBins];
-    double *avgDists = new double[numBins];
+    //double *gamma = new double[numBins];
+    //int *numPairs = new int[numBins];
+    //double *avgDists = new double[numBins];
+    vector<double> gamma (numBins,0);
+    vector<int> numPairs (numBins,0);
+    vector<double> avgDists (numBins,0);
+    /*
     for(int i = 0; i < numBins; i++){
         gamma[i] = 0;
         numPairs[i] = 0;
         avgDists[i] = 0;
     }
-       // - Calculate squared difference of values
-       // - Calculate distance from coordinates
-       // - Add the squared difference to the appropriate distance bin
+    */
+    // - Calculate squared difference of values
+    // - Calculate distance from coordinates
+    // - Add the squared difference to the appropriate distance bin
     for(int i = 0 ; i < allPairs.size(); i++)
     {
-         if(i==0) cout << "First pair values: " << (allPairs[i].pt2)->value << " and " <<
-            (allPairs[i].pt1)->value << "\n";
          double sqdiff = pow((allPairs[i].pt2)->value - (allPairs[i].pt1)->value, 2);
-         if(i==0) cout << "First sqdiff: " << sqdiff << "\n";
          int bin = get_bin(allPairs[i].dist, range, numBins);
-         if(i==0) cout << "First bin: " << bin << "\n";
          gamma[bin] += sqdiff;
          avgDists[bin] += allPairs[i].dist;
          numPairs[bin]++;
@@ -78,39 +75,44 @@ int main(int argc, char* argv[])
         avgDists[i] /= numPairs[i];
     }
     //5. Print output
-    char *outFile = new char(strlen("output.txt") + 1);
-    strcpy(outFile, "output.txt");
-    print_gamma(gamma, numPairs, avgDists, numBins, outFile);
-
+    //char *outFile = new char[strlen("output.txt") + 1];
+    string outFile = "output.txt";
+    //strcpy(outFile, "output.txt");
+    print_gamma(&gamma, &numPairs, &avgDists, numBins, outFile);
+  //cout << "Made it here!\n";
+    /*
     delete[] data;
     delete[] gamma;
     delete[] numPairs;
     delete[] avgDists;
+    delete[] outFile; 
+    */    
     return 0;
 }
 
-int get_num_pairs(char* file)
+int get_num_pairs(string file)
 {
     int numPairs=0;
-    ifstream f(file);
+    ifstream f(file.c_str());
     string line;
     for (int i = 0; getline(f, line); ++i)
     numPairs++;
     return numPairs;  //Assumes no header lines
 }
 
-void read_data(char* file, point* data)
+void read_data(string file,vector<point>* data)
 {
-    ifstream f(file);
+    ifstream f(file.c_str());
     string line;
     for (int i = 0; getline(f, line); ++i)
     {
         istringstream is(line);
-        is >> data[i].value >> data[i].x >>  data[i].y ;
+	point pt;
+        is >> pt.value >> pt.x >>  pt.y ;
+	(*data)[i] = pt;
     }
 }
 
-//double calc_dist(double x1, double x2, double y1, double y2)
 double calc_dist(point* pt1, point* pt2)
 {
     double x_comp = pow(abs(pt2->x - pt1->x), 2);
@@ -118,16 +120,16 @@ double calc_dist(point* pt1, point* pt2)
     return sqrt(x_comp+y_comp);
 }
 
-void register_pairs(int numPoints, point* points, vector<ptPair>* pairs)
+void register_pairs(int numPoints, vector<point>* points, vector<ptPair>* pairs)
 {
     for(int i = 0; i < numPoints; i++)
     {
         for(int j = i+1; j < numPoints; j++)
         {
             ptPair newPair;
-            newPair.pt1 = &points[i];
-            newPair.pt2 = &points[j];
-            newPair.dist = calc_dist(&points[i],&points[j]);
+            newPair.pt1 = &(*points)[i];
+            newPair.pt2 = &(*points)[j];
+            newPair.dist = calc_dist(&(*points)[i],&(*points)[j]);
             pairs->push_back(newPair);
         }
     }
@@ -141,9 +143,9 @@ double* find_dist_range(vector<ptPair>* pairs)
     {
         dists[i] = (*pairs)[i].dist;
     }
-    //cout << "First distance: " << dists[0] << "\n";
     minnmax[0] = *min_element(dists,dists+pairs->size());
     minnmax[1] = *max_element(dists,dists+pairs->size());
+    delete[] dists;
     return minnmax;
 }
 
@@ -157,15 +159,17 @@ int get_bin(double dist, double* range, int numBins)
 
 //}
 
-void print_gamma(double* values, int* counts, double* dists, int numBins, char* filename)
+void print_gamma(vector<double>* values, vector<int>* counts, vector<double>* dists, int numBins, string filename)
 {
     ofstream ofile;
-    ofile.open (filename);
-    ofile << "# \tfrom \tto  \tn_pairs \tav_dist \tsemivariogram \n";
+    ofile.open(filename.c_str());
+    ofile << "#from \tto  \tn_pairs \tav_dist \tsemivariogram \n";
     for(int i = 0; i < numBins; i++)
     {
-        ofile << "?" << "\t" << "?" << "\t" << counts[i] << "\t" <<
-            dists[i] << "\t" << values[i] << "\n";
+        ofile << "?" << "\t" << "?" << "\t" << (*counts)[i] << "\t" <<
+            (*dists)[i] << "\t" << (*values)[i] << "\n";
     }
+    cout << "About to close file\n";
     ofile.close();
+    cout << "Closed file\n";
 }
