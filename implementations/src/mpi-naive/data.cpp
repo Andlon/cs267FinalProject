36336c851d8c63ed04783c13c573ee26419ac118
Andreas Longva <andreas.b.longva@gmail.com>
2015-04-19 00:08:51 -0700

    Added exception thrown when failure to open input file

--- implementations/src/mpi-naive/data.cpp
+++ implementations/src/mpi-naive/data.cpp
@@ -7,6 +7,9 @@ std::vector<data_point> read_file_data(const std::string &path)
     std::vector<data_point> points;
     std::ifstream file(path.c_str());
 
+    if (!file)
+        throw std::ios_base::failure("Failed to open input file");
+
     for (std::string line; std::getline(file, line);)
     {
         std::istringstream stream(line);
